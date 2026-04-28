bfdb1ac40c0e1cfa3966c5bff04dc97dd8686361
jjaemun <jjaemuuu@gmail.com>
2026-04-28 23:01:26 +0200

    draft

---
+++ include/ffc/types.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+
+#include "ffc/core/types.hpp"
+
+
+namespace ffc {
+    using core::usize;
+    using core::isize;
+
+    using core::u8;
+    using core::u16;
+    using core::u32;
+    using core::u64;
+
+    using core::i8;
+    using core::i16;
+    using core::i32;
+    using core::i64;
+
+    using core::f32;
+    using core::f64;
+
+    using core::c64;
+    using core::c128;
+}
